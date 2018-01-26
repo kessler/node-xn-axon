282a66b73592fc27779675aa42d95cd94659338d
kessler <yanivk@gmail.com>
2018-01-27 01:00:56 +0200

    complete overhaul

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 // specify an axon complaint port or address
 const server = require('./index').server(3000)
 
-server.addModule('fs')
-server.addApi('ping', '0.0.1', (callback) => {
+server.addApiFunction('ping', (data, callback) => {
+	console.log('ping')
 	callback(null, 'pong')
 })
 
@@ -10,7 +10,7 @@ const client = require('./index').client(3000)
 
 client.refresh((err, rpc) => {
 	if (err) return console.error(err)
-	rpc.ping((err, result) => {
+	rpc.ping(1, (err, result) => {
 		if (err) return console.error(err)
 		console.log(result) // prints pong
 	})
